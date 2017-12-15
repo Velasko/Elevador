aa399c44eb29f477d42f9dd0cc5349a9b5d03cee
rhanielmx <rhanielmag.xavier@gmail.com>
2017-12-14 23:04:37 -0200

    Update som.h

--- som.h
+++ som.h
@@ -3,7 +3,7 @@
 
 #include "stm32f1xx_hal.h"
 
-repAndarUni(unsigned const char uniAndar, unsigned const char audioAndar);
-repAndarMult(unsigned const char uniAndar, unsigned const char dezAndar, unsigned const char audioAndar);
+void repAndarUni(unsigned const char uniAndar, unsigned const char audioAndar);
+void repAndarMult(unsigned const char uniAndar, unsigned const char dezAndar, unsigned const char audioAndar);
 
 #endif
