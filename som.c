46936523bd09a2ed6a4fd1d8986b2469f6a2678b
rhanielmx <rhanielmag.xavier@gmail.com>
2017-12-18 22:45:54 -0300

    Ideia inicial, não foi para a frente
    Ideia inicial para reproduzir sons fora do interrupt, mas acabamos descobrindo que nao dá tão certo

--- som.c
+++ som.c
@@ -1,6 +1,10 @@
 #include "som.h"
 #include "stm32f1xx_hal.h"
 
+/*
+Função que recebe como parâmetro dois vetores e reproduz o áudio correspondente a eles.
+Utilizada para reproduzir os andares de 1 a 9
+*/
 void repAndarUni(unsigned const char uniAndar, unsigned const char audioAndar){
 		for(int i = 0; i < sizeof(uniAndar);i++){
 			GPIOB->ODR=uniAndar[i];
@@ -11,6 +15,10 @@ void repAndarUni(unsigned const char uniAndar, unsigned const char audioAndar){
 		}
 }
 
+/*
+Função que recebe como parâmetro dois vetores e reproduz o áudio correspondente a eles.
+Utilizada para reproduzir os andares de 10 a 24
+*/
 void repAndarMult(unsigned const char uniAndar, unsigned const char dezAndar, unsigned const char audioAndar){
 		for(int i = 0; i < sizeof(dezAndar);i++){
 			GPIOB->ODR=dezAndar[i];
